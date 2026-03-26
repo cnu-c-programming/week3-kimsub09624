3e8c6e024b0be27a0061dbc166e20eb3c2082e7b
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-26 19:52:25 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main() {
-
-    return 0;
+void increment(int x) {
+	++x;
+	printf("%d\n", x);
 }
+
+int main() {
+	int x = 10;
+	
+	printf("%d\n", x);
+	increment(x);
+	printf("%d\n", x);
+
+	return 0;
+}
+
+
