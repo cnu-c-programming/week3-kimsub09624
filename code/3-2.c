36c9c94c68d6d57eacc335c456ba24069ebf201d
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-26 19:55:00 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+int global_var = 0;
+
+void test_scope() {
+	static int static_var = 0;
+	int local_var = 0;
+
+	global_var++;
+	static_var++;
+	local_var++;
+
+	printf("global_var: %d\n", global_var);
+	printf("static_var: %d\n", static_var);
+	printf("locar_var: %d\n", local_var);
+
+}
+
 int main() {
 
-    return 0;
+	test_scope();
+	printf("\n");
+	test_scope();
+	printf("\n");
+	test_scope();
+
+	return 0;
 }
