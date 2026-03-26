21048f2d0d6aec98b3309688a179774dc853f9d4
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-26 19:55:35 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+int max_of(int count, ...) {
+	va_list list;
+	va_start(list, count);
+	int max = 0;
+	
+	for(int i = 0; i < count; i++) {
+		int num = va_arg(list, int);
+		if(max <= num) max = num;
+	}
+	
+	va_end(list);
 
-    return 0;
+	return max;
 }
+
+int main() {
+	printf("%d\n", max_of(4, 5, 12, 3, 9));
+	printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+	printf("%d\n", max_of(3, 100, 200, 300));
+	return 0;
+}
+
