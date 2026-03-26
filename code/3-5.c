b192a7c1b5acae84b5ef55ef1062bb69d86d1304
S202102611 <162554640+kimsub09624@users.noreply.github.com>
2026-03-26 19:55:56 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int main() {
+void my_sum(char c, int count, ...) {
+	va_list lst;
+	va_start(lst, count);
 
-    return 0;
+	switch(c) {
+		case 'S':
+			for(int i = 0; i < count; i++) {
+				printf("%s ", va_arg(lst, char *));
+			}
+			break;
+
+		case 'C': 
+			for(int i = 0; i < count; i++) {
+				printf("%c", va_arg(lst, int));
+			}
+			break;
+
+		case 'D':
+			int total = 0;
+			for(int i = 0; i < count; i++) {
+				total += va_arg(lst, int);
+			}
+			printf("%d", total);
+			break;
+
+	}
+	printf("\n");
+	
+}
+
+int main() {
+	my_sum('S', 2, "Hello", "World");
+	my_sum('C', 3, 'C', 'N', 'U');
+	my_sum('D', 4, 10, 20, 30, 40);
+
+	return 0;
 }
